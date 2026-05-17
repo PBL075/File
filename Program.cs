2e6684ab7a334cc572a622003f80ad5924d76014
PBL075 <pavelgorea75@gmail.com>
2026-05-17 19:54:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ss_file
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nume = "scrisoare.txt";
+            string text = "buna ziua!!";
+
+            File.WriteAllText (nume , text);
+
+            if (File.Exists(nume))
+            {
+                string textul = File.ReadAllText(nume);
+                Console.WriteLine(textul);
+            }
+
+            
+        }
+    }
+}
